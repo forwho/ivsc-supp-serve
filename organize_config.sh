aef9fe17b98cc3ae6b09f895b5f3f6ee398e8a13
forwho <hwjbupt@gmail.com>
2024-04-11 14:30:29 +0100

    fix buhs

--- organize_config.sh
+++ organize_config.sh
@@ -1,2 +1,3 @@
-spath=/shared/su_group/User/md1weih/CamCAN/cc700/mri/pipeline/release004/BIDS_20190411/
-tpath=/mnt/parscratch/users/md1weih/indi-sc/camcan
+spath1=/shared/su_group/User/md1weih/HCP_data/HCP_Aging/imagingcollection01/
+spath2=/shared/su_group/User/md1weih/CamCAN/cc700/mri/pipeline/release004/BIDS_20190411/
+tpath=/mnt/parscratch/users/md1weih/indi-sc/data/camcan
